bcdc922e31ca51f6ea1c491b5c3f73b4b5eb2953
mourad244 <mouradvilla2@gmail.com>
2021-01-11 21:03:26 +0100

    add support for connStr

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -1,14 +1,22 @@
 const logger = require("../startup/logging");
 const mongoose = require("mongoose");
 const config = require("config");
+require("dotenv").config();
+
 module.exports = function () {
-  const db = config.get("db");
   mongoose
-    .connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    })
+    .connect(
+      "mongodb+srv://" +
+        process.env.DB_USERNAME +
+        ":" +
+        process.env.DB_PASSWORD +
+        "@cluster0.x0xqe.mongodb.net/psapi?retryWrites=true&w=majority" /*||  config.get("db") */,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      }
+    )
     .then(() => logger.info("Connected to MongoDB..."));
 };
