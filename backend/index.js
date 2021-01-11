f7286cb81e4467e8b7ba7f7022ad1844ec26a57c
mourad244 <mouradvilla2@gmail.com>
2021-01-11 16:05:37 +0100

    before starting deployment

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
-const winston = require("winston");
+// const winston = require("winston");
 const express = require("express");
-const logger = require("./startup/logging");
 const app = express();
 
+const logger = require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3900;
 app.listen(port, () => logger.info(`Listening on port ${port}...`));
