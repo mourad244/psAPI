40c5fd20a3af91b9f417cdea02bcb8ad9fe66f13
mourad244 <mouradvilla2@gmail.com>
2021-01-11 18:53:43 +0100

    add support for connStr

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 const winston = require("winston");
 const express = require("express");
+const logger = require("./startup/logging");
+const config = require("config");
 const app = express();
 
-const logger = require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
 
-const port = process.env.PORT || 3900;
-app.listen(port, () => logger.info(`Listening on port ${port}...`));
+const port = process.env.PORT || config.get("port");
+const server = app.listen(port, () =>
+  logger.info(`Listening on port ${port}...`)
+);
+
+module.exports = server;
