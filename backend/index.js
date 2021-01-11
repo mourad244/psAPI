06b501d49ed4dd2807b6fbc8fb6c31577830d6ed
mourad244 <mouradvilla2@gmail.com>
2021-01-11 20:21:13 +0100

    add support for connStr

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ require("./startup/validation")();
 require("./startup/prod")(app);
 
 const port = process.env.PORT || config.get("port");
+if (process.env.ENV === "dev") console.log("done");
 const server = app.listen(port, () =>
   logger.info(`Listening on port ${port}...`)
 );
