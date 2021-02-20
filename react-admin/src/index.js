019e00b86fceb824cbab68f5cc2fcb6baf2069ff
mourad244 <mouradvilla2@gmail.com>
2021-02-20 16:08:41 +0100

    add more

--- react-admin/src/index.js
+++ react-admin/src/index.js
@@ -7,6 +7,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
+console.log(process.env);
 ReactDOM.render(
   <BrowserRouter>
     <App />
